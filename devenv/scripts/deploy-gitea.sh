960ea2b192fb0dd42e7cbf3356574070427358a8
Georg Federmann <georg.federmann@gmail.com>
2020-04-17 13:10:48 +0200

    fix gitea repo base url in env config

--- devenv/scripts/deploy-gitea.sh
+++ devenv/scripts/deploy-gitea.sh
@@ -21,6 +21,7 @@ docker container run -d --rm --name db \
     mysql:5.7
 
 mysql_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' db)
+echo "mysql ip is ${mysql_ip}"
 
 docker container run -d --rm --name gitea \
     -e DB_TYPE=mysql \
@@ -36,6 +37,9 @@ docker container run -d --rm --name gitea \
     --volume /etc/localtime:/etc/localtime:ro \
     gitea/gitea:latest
 
+gitea_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' gitea)
+echo "gitea ip is ${gitea_ip}"
+
 echo "Waiting for gitea container to come online."
 sleep 25
 counter=0
@@ -47,7 +51,8 @@ docker container exec gitea bash -c "gitea migrate"
 docker container exec gitea bash -c "gitea admin create-user --username cd_user --password cd_passworD1 --email cd_user@example.com --admin"
 
 # create test repositories
-gitea_url=172.17.0.1:8080
+gitea_url=${gitea_ip}:8080
+sed -i 's|^REPO_BASE=http://172.17.0.1:8080/cd_user$|REPO_BASE=http://${gitea_url}/cd_user/simpleapp.git|' ${BASH_SOURCE%/*}/../../ods-config/ods-core.env
 read -n1 -r -p "Now, log into gitea under http://${gitea_url} using credentials cd_user:cd_passworD1 and press SPACE when done."
 curl -X POST "http://cd_user:cd_passworD1@${gitea_url}/api/v1/user/repos" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"auto_init\": true, \"default_branch\": \"master\", \"description\": \"test setup for gitea\", \"name\": \"ods-core\", \"private\": false}" | jq .
 curl -X POST "http://cd_user:cd_passworD1@${gitea_url}/api/v1/user/repos" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"auto_init\": true, \"default_branch\": \"master\", \"description\": \"test setup for gitea\", \"name\": \"ods-configuration\", \"private\": false}" | jq .
